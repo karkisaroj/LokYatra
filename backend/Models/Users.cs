24a1e5b33fa5d69f82c84f724f4bc5792c2af9e8
saorj karki <karkisaroj3012@gmail.com>
2025-12-18 21:28:04 +0545

    Installed new packages from Nuget for .net apis

---
+++ backend/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userId { get; set; }
+        [Required]
+        public String name { get; set; }
+
+        [Required]
+        public string address {  get; set; }
+    }
+}
