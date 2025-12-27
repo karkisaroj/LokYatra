d6043a8c14cb64b128b22134e280d3bbb9ef2a1a
saorj karki <karkisaroj3012@gmail.com>
2025-12-27 18:59:37 +0545

    Recreated backend project

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
