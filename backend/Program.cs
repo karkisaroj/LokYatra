a1bc6ed776f636300ee97c0d04a8dbc2e14c21af
saorj karki <karkisaroj3012@gmail.com>
2026-01-12 17:35:14 +0545

    Changed the localhost service for web

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,49 +6,57 @@ using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("UserDatabase"))
 );
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["AppSettings:Issuer"],
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["AppSettings:Audience"],
-        ValidateLifetime = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)),
-        ValidateIssuerSigningKey = true
-    };
-});
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["AppSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["AppSettings:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)
+            ),
+            ValidateIssuerSigningKey = true,
+            RoleClaimType = "role"
+        };
+    });
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFlutterWeb",
-        policy => policy
-            .AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod());
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 });
+
 builder.Services.AddScoped<IAuthService, AuthService>();
+
 var app = builder.Build();
 
-app.UseCors("AllowFlutterWeb");
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-builder.WebHost.UseUrls("http://0.0.0.0:5257", "https://localhost:7200");
+
+builder.WebHost.UseUrls("http://localhost:5257", "https://localhost:7200");
+
 app.UseAuthentication();
-//app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
